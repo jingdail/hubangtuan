820e82b13d0e310820502d72e09a9123be0c68b3
zhang <1269078063@qq.com>
2020-08-07 11:51:05 +0800

    修改了配置文件格式

--- common/config.js
+++ common/config.js
@@ -1,14 +1,14 @@
 // const ROOTPATH = "https://www.gomyorder.cn";
 const Base = "https://app.wolewan.com/";
 module.exports = {
-	list: Base+'api/lists/',
-	category: Base+'json/categories.json',
-    publish: Base+"api/publish",
-    uploadpic: Base+"api/pic",
-	sms: Base+"api/sms",
-	mobile: Base+"api/mobile",
-	jinbi: Base+"api/jinbi",
-	logon: Base+"logon",
-	daka: Base+'daka',
-	my: Base+'my'
+	list: Base + 'api/lists/',
+	category: Base + 'json/categories.json',
+	publish: Base + "api/publish",
+	uploadpic: Base + "api/pic",
+	sms: Base + "api/sms",
+	mobile: Base + "api/mobile",
+	jinbi: Base + "api/jinbi",
+	logon: Base + "logon",
+	daka: Base + 'daka',
+	my: Base + 'my'
 };
