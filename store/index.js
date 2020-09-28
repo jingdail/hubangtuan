e4dfffd12fd5c7821ae6567d08458871991a6cb2
zhang <1269078063@qq.com>
2020-09-28 14:57:10 +0800

    .

---
+++ store/index.js
@@ -0,0 +1,31 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+Vue.use(Vuex)
+const store = new Vuex.Store({
+    state: {
+    	hasLogin: false, // 登录状态
+    	userInfo:{}
+    },
+    mutations: {
+		login(state, provider) {
+			console.log("登录了");
+			console.log(provider)
+			state.hasLogin = true;
+			state.userInfo = provider
+			uni.setStorage({
+				key:"userInfo",
+				data:provider
+			})
+		},
+		logout(state) {
+			state.hasLogin = false;
+			state.openid = null;
+			state.userinfo = {};
+			uni.removeStorage({
+				key:"userInfo"
+			})
+		}
+	},
+    actions: {}
+})
+export default store
